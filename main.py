53a2f18d52ef7a49f35a1bba188b9c7a28c8d574
Flauver <thedarkwhite@126.com>
2026-04-18 12:26:39 +0800

    增加 derive

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-﻿from os import replace
-from re import *
+﻿from re import *
+from dataclasses import dataclass
+
+@dataclass
+class 拼运:
+    type: str
+    fr: str
+    to: str
+
+    def apply(self, inputs: list[str]) -> list[str]:
+        results: list[str] = []
+        for 音节 in inputs:
+            match self.type:
+                case 'xform':
+                    results.append(sub(self.fr, self.to, 音节))
+                case 'derive':
+                    results.append(音节)
+                    results.append(sub(self.fr, self.to, 音节))
+        
+        return results
+
 
 当量: dict[str, float] = {}
 
@@ -17,53 +36,60 @@ def 分割(text: str) -> tuple[str, str]:
         return (group.group(1), group.group(2))
     return ('', text)
 
-def 拼运(text: str) -> str:
+def 应用拼运(text: str) -> list[str]:
     声, 韵  = 分割(text)
     声 = 声.replace('zh', 'v')
     声 = 声.replace('ch', 'i')
     声 = 声.replace('sh', 'u')
-    拼韵 = 韵.replace('iong', 's')
-    拼韵 = 拼韵.replace('iang', 'd')
-    拼韵 = 拼韵.replace('uang', 'd')
-    拼韵 = 拼韵.replace('uan', 'r')
-    拼韵 = 拼韵.replace('van', 'r')
-    拼韵 = 拼韵.replace('ing', 'y')
-    拼韵 = 拼韵.replace('uai', 'y')
-    拼韵 = 拼韵.replace('ong', 's')
-    拼韵 = 拼韵.replace('eng', 'g')
-    拼韵 = 拼韵.replace('ang', 'h')
-    拼韵 = 拼韵.replace('iao', 'c')
-    拼韵 = 拼韵.replace('ian', 'm')
-    拼韵 = 拼韵.replace('iu', 'q')
-    拼韵 = 拼韵.replace('ia', 'w')
-    拼韵 = 拼韵.replace('ua', 'w')
-    拼韵 = 拼韵.replace('ue', 't')
-    拼韵 = 拼韵.replace('ve', 't')
-    拼韵 = 拼韵.replace('uo', 'o')
-    拼韵 = 拼韵.replace('un', 'p')
-    拼韵 = 拼韵.replace('en', 'f')
-    拼韵 = 拼韵.replace('an', 'j')
-    拼韵 = 拼韵.replace('ao', 'k')
-    拼韵 = 拼韵.replace('ai', 'l')
-    拼韵 = 拼韵.replace('ei', 'z')
-    拼韵 = 拼韵.replace('ie', 'x')
-    拼韵 = 拼韵.replace('ui', 'v')
-    拼韵 = 拼韵.replace('ou', 'b')
-    拼韵 = 拼韵.replace('in', 'n')
-    拼韵 = 拼韵.replace('ng', 'g')
+    拼韵: list[str] = 拼运(type='xform', fr='iong', to='s').apply([韵])
+    拼韵  = 拼运(type='xform', fr='iang', to='d').apply(拼韵)
+    拼韵: list[str] = 拼运(type='xform', fr='iang', to='d').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='uang', to='d').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='uan', to='r').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='van', to='r').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ing', to='y').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='uai', to='y').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ong', to='s').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='eng', to='g').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ang', to='h').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='iao', to='c').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ian', to='m').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='iu', to='q').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ia', to='w').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ua', to='w').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ue', to='t').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ve', to='t').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='uo', to='o').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='un', to='p').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='en', to='f').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='an', to='j').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ao', to='k').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ai', to='l').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ei', to='z').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ie', to='x').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ui', to='v').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ou', to='b').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='in', to='n').apply(拼韵)
+    拼韵 = 拼运(type='xform', fr='ng', to='g').apply(拼韵)
+    拼韵 = 拼运(type='derive', fr='i', to='d').apply(拼韵)
     if 声 == '':
         if len(韵) == 2:
-            return 韵
+            return [韵]
         else:
-            return 韵[0] + 拼韵
-    return 声 + 拼韵
+            return [韵[0] + x for x in 拼韵]
+    return [声 + x for x in 拼韵]
 
 列表: list[tuple[str, int, float]] = []
 
 with open('freq.txt') as f:
     text = f.read()
 for 行 in findall(r'^([a-z]+)\t(\d+)$', text, flags=M):
-    列表.append((拼运(行[0]), int(行[1]), 当量[拼运(行[0])]))
+    行: tuple[str, str]
+    (行)
+    结果 = [(x, int(行[1]), 当量[x]) for x in 应用拼运(行[0])]
+    print(行, 结果)
+    m = min([x[2] for x in 结果])
+    列表.append(next((x for x in 结果 if x[2] == m)))
 
 列表 = sorted(列表, key=lambda x: x[1] / 67028000 + x[2] / (-1.4))
 
